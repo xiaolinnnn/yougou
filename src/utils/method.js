937122f9fcc2e908c989bd5234bc812b1a4ca096
xiaolin <939158029@qq.com>
2018-07-03 20:49:56 +0800

    2018-07-03

--- src/utils/method.js
+++ src/utils/method.js
@@ -1,5 +1,6 @@
 
 // 抽取 基地址
+// 配置
 var baseURL = 'https://127.0.0.1/';
 // 写封装的东西
 var method = {
@@ -9,12 +10,15 @@ var method = {
       method
       success
       fail
+      函数的上下文调用模式
+      apply call bind
   */
   request:(opt)=>{
     wx.request({
       url: `${baseURL}${opt.url}`||'',
       method: opt.method||'get',
       success:opt.success||((backData)=>{console.log(backData);}),
+      header:opt.header||{},
       data:opt.data||{},
       fail: opt.fail||(()=>{})
     })
